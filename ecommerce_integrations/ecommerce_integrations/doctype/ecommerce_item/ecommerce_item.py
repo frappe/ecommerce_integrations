2e1d6f73a0e9d1ab080617f1a6673de4456efd88
Ankush Menat <ankush@iwebnotes.com>
2021-04-20 21:09:09 +0530

    feat: Ecommerce item DocType
    This DocType would be used as bridge table for correlating items on
    ecommerce integrations with ERPNext items.

--- ecommerce_integrations/ecommerce_integrations/doctype/ecommerce_item/ecommerce_item.py
+++ ecommerce_integrations/ecommerce_integrations/doctype/ecommerce_item/ecommerce_item.py
@@ -6,14 +6,78 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 
+from typing import Optional
+
 class EcommerceItem(Document):
 
 	def validate(self):
-		already_exists =  frappe.db.exists("Ecommerce Item", {
+		self.check_mandatory()
+		self.check_duplicate()
+
+	def check_mandatory(self):
+		if self.has_variants and ((not self.variant_id) or (not self.variant_of)):
+			raise frappe.ValidationError("variant_id and variant_of required")
+
+	def check_duplicate(self):
+		filter = {
 				"erpnext_item_code": self.erpnext_item_code,
 				"integration": self.integration,
 				"integration_item_code" : self.integration_item_code
-			})
+			}
 
-		if already_exists:
+		if self.has_variants:
+			filter.update({ "variant_id": self.variant_id })
+
+		if frappe.db.exists("Ecommerce Item", filter):
 			raise frappe.DuplicateEntryError(_("Ecommerce item already exists"))
+
+
+
+def is_synced(integration: str, integration_item_code: str,
+		variant_id: Optional[str]= None) -> bool:
+	""" Check if item is synced with integration.
+
+		variant_id is optional. Use variant_id to check if particular variant is synced.
+		E.g.
+			integration: shopify,
+			integration_item_code: TSHIRT
+			variant_id: red_t_shirt
+	"""
+
+	filter = {
+			"integration" : integration,
+			"integration_item_code" : integration_item_code
+		}
+	if variant_id:
+		filter.update({ "variant_id": variant_id })
+
+	return bool(frappe.db.exists("Ecommerce Item", filter))
+
+
+def get_erpnext_item(integration: str, integration_item_code: str,
+		variant_id: Optional[str]= None):
+
+	filter = {
+			"integration" : integration,
+			"integration_item_code" : integration_item_code
+		}
+	if variant_id:
+		filter.update({ "variant_id": variant_id })
+
+	item_code = frappe.db.get_value("Ecommerce Item", filter, fieldname="erpnext_item_code")
+
+	if item_code:
+		return frappe.get_doc("Item", item_code)
+
+	return None
+
+
+def get_integration_item_code(erpnext_item_code: str, integration: str) -> str:
+	filter = {
+			"integration" : integration,
+			"erpnext_item_code" : erpnext_item_code
+		}
+
+	integration_item_code = frappe.db.get_value("Ecommerce Item", filter, fieldname="integration_item_code")
+
+	return integration_item_code
--- ecommerce_integrations/ecommerce_integrations/doctype/ecommerce_item/test_ecommerce_item.py
+++ ecommerce_integrations/ecommerce_integrations/doctype/ecommerce_item/test_ecommerce_item.py
@@ -1,10 +1,77 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2021, Frappe and Contributors
 # See license.txt
-from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
+import json
+
+from ecommerce_integrations.ecommerce_integrations.doctype.ecommerce_item import ecommerce_item
 
 class TestEcommerceItem(unittest.TestCase):
-	pass
+
+	def tearDown(self):
+		for d in frappe.get_list("Ecommerce Item"):
+			frappe.get_doc("Ecommerce Item", d.name).delete()
+
+
+	def test_duplicate(self):
+		self._create_doc()
+		self.assertRaises(frappe.DuplicateEntryError, self._create_doc)
+
+
+	def test_duplicate_variants(self):
+		self._create_variant_doc()
+		self.assertRaises(frappe.DuplicateEntryError, self._create_variant_doc)
+
+
+
+	def test_is_synced(self):
+		self._create_doc()
+		self.assertTrue(ecommerce_item.is_synced("Shopify", "T-SHIRT"))
+		self.assertFalse(ecommerce_item.is_synced("Shopify", "UNKNOWN ITEM"))
+
+
+	def test_is_synced_variant(self):
+		self._create_variant_doc()
+		self.assertTrue(ecommerce_item.is_synced("Shopify", "T-SHIRT", "T-SHIRT-RED" ))
+		self.assertFalse(ecommerce_item.is_synced("Shopify", "T-SHIRT", "Unknown variant"))
+
+
+	def test_get_erpnext_item(self):
+		self._create_doc()
+		a = ecommerce_item.get_erpnext_item("Shopify", "T-SHIRT")
+		b = frappe.get_doc("Item", "Test Item")
+		self.assertEqual(a.name, b.name)
+		self.assertEqual(a.item_code, b.item_code)
+
+		unknown = ecommerce_item.get_erpnext_item("Shopify", "Unknown item")
+		self.assertEqual(unknown, None)
+
+	def test_get_erpnext_item_variant(self):
+		self._create_variant_doc()
+		a = ecommerce_item.get_erpnext_item("Shopify", "T-SHIRT", "T-SHIRT-RED")
+		b = frappe.get_doc("Item", "Test Item Variant")
+		self.assertEqual(a.name, b.name)
+		self.assertEqual(a.item_code, b.item_code)
+
+
+	def _create_doc(self):
+		"""basic test for creation of ecommerce item"""
+		frappe.get_doc({
+				"doctype": "Ecommerce Item",
+				"integration" : "Shopify",
+				"integration_item_code" : "T-SHIRT",
+				"erpnext_item_code" : "Test Item",
+			}).insert()
+
+	def _create_variant_doc(self):
+		"""basic test for creation of ecommerce item"""
+		frappe.get_doc({
+				"doctype": "Ecommerce Item",
+				"integration" : "Shopify",
+				"integration_item_code" : "T-SHIRT",
+				"erpnext_item_code" : "Test Item Variant",
+				"has_variants": 1,
+				"variant_id": "T-SHIRT-RED",
+				"variant_of": "Test Item",
+			}).insert()
