30178be2aa49aa5927782fa24bf42e0617ae2fb5
Ankush Menat <ankush@iwebnotes.com>
2021-05-13 11:43:23 +0530

    fix: add js to warn about use of old settings

---
+++ ecommerce_integrations/public/js/shopify_old_settings.js
@@ -0,0 +1,8 @@
+
+frappe.ui.form.on('Shopify Settings', {
+	onload_post_render: function(frm) {
+		let msg = __("You have Ecommerce Integration app installed.") + " "
+		msg += __("This setting page refers to old Shopify connector.")
+		frappe.msgprint(msg);
+	}
+});
