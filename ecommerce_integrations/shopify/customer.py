f3ed2c13c9c64365a047808fa1277b9cda93c97c
Ankush Menat <ankush@iwebnotes.com>
2021-06-16 15:42:20 +0530

    chore: formatting

--- ecommerce_integrations/shopify/customer.py
+++ ecommerce_integrations/shopify/customer.py
@@ -27,7 +27,6 @@ class ShopifyCustomer(EcommerceCustomer):
 		customer_group = self.setting.customer_group
 		super().sync_customer(customer_name, customer_group)
 
-
 	def create_customer_address(self, customer_name, customer_dict: Dict[str, Any]) -> None:
 		"""Create customer address(es) using Customer dict provided by shopify."""
 		shopify_address = customer_dict.get("default_address", {})
@@ -49,7 +48,9 @@ class ShopifyCustomer(EcommerceCustomer):
 
 		super().create_customer_address(address_fields)
 
-	def create_additional_address(self, customer_name, customer_mail, type, shopify_address: Dict[str, Any]) -> None:
+	def create_additional_address(
+		self, customer_name, customer_mail, type, shopify_address: Dict[str, Any]
+	) -> None:
 		"""Create customer address(es) using Customer dict provided by shopify."""
 		# map shopify address fields to ERPNext
 		address_fields = {
@@ -67,10 +68,12 @@ class ShopifyCustomer(EcommerceCustomer):
 
 		super().create_customer_address(address_fields)
 
-	def update_additional_address(self, customer_name, customer_mail, type, shopify_address: Dict[str, Any]) -> None:
+	def update_additional_address(
+		self, customer_name, customer_mail, type, shopify_address: Dict[str, Any]
+	) -> None:
 		oldAddress = self.get_customer_address_doc(customer_name, _(type))
 
-		if(not oldAddress):
+		if not oldAddress:
 			self.create_additional_address(customer_name, customer_mail, type, shopify_address)
 		else:
 			oldAddress.address_line1 = shopify_address.get("address1") or "Address 1"
@@ -88,29 +91,20 @@ class ShopifyCustomer(EcommerceCustomer):
 			"status": "Passive",
 			"first_name": first_name,
 			"last_name": last_name,
-			"unsubscribed": not marketing
+			"unsubscribed": not marketing,
 		}
 
-		if(customer_mail):
-			contact_fields["email_ids"] =  [{
-				"email_id": customer_mail,
-				"is_primary": True
-			}]
+		if customer_mail:
+			contact_fields["email_ids"] = [{"email_id": customer_mail, "is_primary": True}]
 
 		phone_nos = []
-		if(phone):
-			phone_nos.append({
-				"phone": phone,
-				"is_primary_phone": True
-			})
+		if phone:
+			phone_nos.append({"phone": phone, "is_primary_phone": True})
 
-		if(mobile):
-			phone_nos.append({
-				"phone": mobile,
-				"is_primary_phone": True
-			})
+		if mobile:
+			phone_nos.append({"phone": mobile, "is_primary_phone": True})
 
-		if(len(phone_nos) > 0):
+		if len(phone_nos) > 0:
 			contact_fields["phone_nos"] = phone_nos
 
 		super().create_customer_contact(contact_fields)
