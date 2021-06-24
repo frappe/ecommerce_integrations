2ade7790a4776ee157ed67af5ed2c2dd65cb5deb
Ankush Menat <ankush@iwebnotes.com>
2021-06-24 16:40:39 +0530

    feat: validate incoming SO items

--- ecommerce_integrations/unicommerce/tests/test_order.py
+++ ecommerce_integrations/unicommerce/tests/test_order.py
@@ -1,11 +1,6 @@
-import json
-
-import frappe
-import responses
 from frappe.test_runner import make_test_records
 
-from ecommerce_integrations.unicommerce.constants import MODULE_NAME
-from ecommerce_integrations.unicommerce.order import fetch_new_orders
+from ecommerce_integrations.unicommerce.order import _validate_item_list
 from ecommerce_integrations.unicommerce.tests.test_client import TestCaseApiClient
 
 
@@ -15,25 +10,10 @@ class TestUnicommerceProduct(TestCaseApiClient):
 		super().setUpClass()
 		make_test_records("Unicommerce Channel")
 
-	@responses.activate
-	def test_fetch_new_order(self):
-		def sales_order_mock(request):
-			payload = json.loads(request.body)
-			resp_body = self.load_fixture(f"order-{payload['code']}")
-			headers = {}
-			return (200, headers, json.dumps(resp_body))
+	def test_validate_item_list(self):
+		order_files = ["order-SO5905", "order-SO5906", "order-SO5907"]
+		items_list = [{"MC-100", "TITANIUM_WATCH"}, {"MC-100",}, {"MC-100", "TITANIUM_WATCH"}]
 
-		responses.add_callback(
-			responses.POST,
-			"https://demostaging.unicommerce.com/services/rest/v1/oms/saleorder/get",
-			callback=sales_order_mock,
-			content_type="application/json",
-		)
-
-		responses.add(
-			responses.POST,
-			"https://demostaging.unicommerce.com/services/rest/v1/oms/saleOrder/search",
-			status=200,
-			json=self.load_fixture("so_search_results"),
-		)
-		fetch_new_orders(client=self.client, force=True)
+		for order_file, items in zip(order_files, items_list):
+			order = self.load_fixture(order_file)["saleOrderDTO"]
+			self.assertEqual(items, _validate_item_list(order, client=self.client))
