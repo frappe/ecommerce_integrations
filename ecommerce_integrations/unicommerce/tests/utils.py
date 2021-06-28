9c14e90363a9430bcb42e7e12bf3135ee16a4f16
Ankush Menat <ankush@iwebnotes.com>
2021-06-28 15:14:34 +0530

    feat: sync customer

--- ecommerce_integrations/unicommerce/tests/utils.py
+++ ecommerce_integrations/unicommerce/tests/utils.py
@@ -16,6 +16,7 @@ class TestCase(unittest.TestCase):
 		"access_token": "AUTH_TOKEN",
 		"is_enabled": 1,
 		"enable_inventory_sync": 1,
+		"default_customer_group": "Individual",
 		"warehouse_mapping": [
 			{"unicommerce_facility_code": "A", "erpnext_warehouse": "Stores - WP", "enabled": 1},
 			{"unicommerce_facility_code": "B", "erpnext_warehouse": "Work In Progress - WP", "enabled": 1},
