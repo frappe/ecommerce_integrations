93806770eed549fb2c148f9194c3f9519a640788
Ankush Menat <ankush@iwebnotes.com>
2021-07-13 11:26:51 +0530

    fix: validation for company links in channel conf

--- ecommerce_integrations/unicommerce/doctype/unicommerce_channel/unicommerce_channel.py
+++ ecommerce_integrations/unicommerce/doctype/unicommerce_channel/unicommerce_channel.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class UnicommerceChannel(Document):
-	pass
+	def validate(self):
+		self.__check_compnay()
+
+	def __check_compnay(self):
+		company_fields = {
+			"warehouse": "Warehouse",
+			"fnf_account": "Account",
+			"igst_account": "Account",
+			"cgst_account": "Account",
+			"sgst_account": "Account",
+			"ugst_account": "Account",
+		}
+
+		for field, doctype in company_fields.items():
+			if self.company != frappe.db.get_value(doctype, self.get(field), "company"):
+				frappe.throw(
+					_("{}: {} does not belong to company {}").format(doctype, self.get(field), self.company)
+				)
