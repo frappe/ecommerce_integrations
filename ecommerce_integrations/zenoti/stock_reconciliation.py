8889a8658cd68c02f609c5fcfce93bfb7608f85a
Afshan <afshan13k@gmail.com>
2021-08-23 19:49:29 +0530

    fix: don't add unchanged items

--- ecommerce_integrations/zenoti/stock_reconciliation.py
+++ ecommerce_integrations/zenoti/stock_reconciliation.py
@@ -1,9 +1,14 @@
 import frappe
 from frappe import _
-from frappe.utils import now
+from frappe.utils import now, flt
 from ecommerce_integrations.zenoti.utils import api_url, check_for_item, get_cost_center, get_warehouse, make_api_call, get_center_code
 
+from erpnext.stock.doctype.stock_reconciliation.stock_reconciliation import get_stock_balance_for
+
 def process_stock_reconciliation(list_of_centers, error_logs):
+	purpose = "Stock Reconciliation"
+	if not frappe.get_all("Stock Reconciliation"):
+		purpose = "Opening Stock"
 	for center in list_of_centers:
 		list_for_entry = []
 		stock_quantities_of_products_in_a_center = retrieve_stock_quantities_of_products(center)
@@ -21,7 +26,7 @@ def process_stock_reconciliation(list_of_centers, error_logs):
 				error_logs.append(item_err_msg)
 			if err_msg or len(item_err_msg_list):
 				continue
-			make_stock_reconciliation(list_for_entry, cost_center)
+			make_stock_reconciliation(list_for_entry, cost_center, purpose)
 
 def retrieve_stock_quantities_of_products(center):
 	url = api_url + "inventory/stock?center_id={0}&inventory_date={1}".format(center, now())
@@ -45,15 +50,19 @@ def make_list_for_entry(data, list_for_entry, error_logs):
 			list_for_entry.append(record)
 	return list_for_entry
 
-def make_stock_reconciliation(list_for_entry, cost_center):
+def make_stock_reconciliation(list_for_entry, cost_center, purpose):
 	doc = frappe.new_doc("Stock Reconciliation")
-	doc.purpose = "Stock Reconciliation"
+	doc.purpose = purpose
 	doc.posting_date = frappe.utils.nowdate()
 	doc.posting_time = frappe.utils.nowtime()
 	doc.cost_center = cost_center
 	doc.set('items', [])
+	doc.difference_amount = 0.0
 	add_items_to_reconcile(doc, list_for_entry)
-	doc.insert()
+	items = list(filter(lambda d: changed(d, doc), doc.items))
+	if items:
+		doc.items = items
+		doc.insert()
 
 def add_items_to_reconcile(doc, list_for_entry):
 	for item in list_for_entry:
@@ -61,3 +70,31 @@ def add_items_to_reconcile(doc, list_for_entry):
 		for key, value in item.items():
 			invoice_item[key] = value
 		doc.append("items", invoice_item)
+
+def changed(item, doc):
+	item_dict = get_stock_balance_for(item.item_code, item.warehouse,
+		doc.posting_date, doc.posting_time, batch_no=item.batch_no)
+
+	if ((item.qty is None or item.qty==item_dict.get("qty")) and
+		(item.valuation_rate is None or item.valuation_rate==item_dict.get("rate")) and
+		(not item.serial_no or (item.serial_no == item_dict.get("serial_nos")) )):
+		return False
+	else:
+		# set default as current rates
+		if item.qty is None:
+			item.qty = item_dict.get("qty")
+
+		if item.valuation_rate is None:
+			item.valuation_rate = item_dict.get("rate")
+
+		if item_dict.get("serial_nos"):
+			item.current_serial_no = item_dict.get("serial_nos")
+			if doc.purpose == "Stock Reconciliation" and not item.serial_no:
+				item.serial_no = item.current_serial_no
+
+		item.current_qty = item_dict.get("qty")
+		item.current_valuation_rate = item_dict.get("rate")
+		doc.difference_amount += (flt(item.qty, item.precision("qty")) * \
+			flt(item.valuation_rate or item_dict.get("rate"), item.precision("valuation_rate")) \
+			- flt(item_dict.get("qty"), item.precision("qty")) * flt(item_dict.get("rate"), item.precision("valuation_rate")))
+		return True
