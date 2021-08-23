36b567a67592e657461b96b7419b792d155fd0b2
Afshan <afshan13k@gmail.com>
2021-08-23 21:42:36 +0530

    fix: proceed for center code only when response available

--- ecommerce_integrations/zenoti/stock_reconciliation.py
+++ ecommerce_integrations/zenoti/stock_reconciliation.py
@@ -12,21 +12,22 @@ def process_stock_reconciliation(list_of_centers, error_logs):
 	for center in list_of_centers:
 		list_for_entry = []
 		stock_quantities_of_products_in_a_center = retrieve_stock_quantities_of_products(center)
-		center_code, center_code_err = get_center_code(center)
-		if center_code_err:
-			error_logs.append(center_code_err)
-		make_list_for_entry(stock_quantities_of_products_in_a_center, list_for_entry, error_logs)
-		if list_for_entry and center_code:
-			cost_center, err_msg = get_cost_center(center_code)
-			if err_msg:
-				error_logs.append(err_msg)
-			item_err_msg_list = check_for_item(list_for_entry, item_group="Products")
-			if len(item_err_msg_list):
-				item_err_msg = "\n".join(err for err in item_err_msg_list)
-				error_logs.append(item_err_msg)
-			if err_msg or len(item_err_msg_list):
-				continue
-			make_stock_reconciliation(list_for_entry, cost_center, purpose)
+		if stock_quantities_of_products_in_a_center:
+			center_code, center_code_err = get_center_code(center)
+			if center_code_err:
+				error_logs.append(center_code_err)
+			make_list_for_entry(stock_quantities_of_products_in_a_center, list_for_entry, error_logs)
+			if list_for_entry and center_code:
+				cost_center, err_msg = get_cost_center(center_code)
+				if err_msg:
+					error_logs.append(err_msg)
+				item_err_msg_list = check_for_item(list_for_entry, item_group="Products")
+				if len(item_err_msg_list):
+					item_err_msg = "\n".join(err for err in item_err_msg_list)
+					error_logs.append(item_err_msg)
+				if err_msg or len(item_err_msg_list):
+					continue
+				make_stock_reconciliation(list_for_entry, cost_center, purpose)
 
 def retrieve_stock_quantities_of_products(center):
 	url = api_url + "inventory/stock?center_id={0}&inventory_date={1}".format(center, now())
