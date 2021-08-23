60a136bf827a2fc1e9da396ddc8392e843fd52ea
Afshan <33727827+AfshanKhan@users.noreply.github.com>
2021-08-23 19:58:04 +0530

    fix: Verify if enable perpetual inventory is unchecked and set unchecked if checked (#67)
    * fix: item level discount calculations (#56)
    
    * fix(ux): always hide old settings
    
    * fix: line item level discount calculation
    
    line item level taxes weren't getting considered correctly.
    
    Soln: remove global discounts and remove shopify's allocated basic
    discounts.
    
    * fix: don't add unchanged items
    
    * fix: Verify if enable perpetual inventory is unchecked and set unchecked if checked
    
    Co-authored-by: Ankush Menat <ankush@iwebnotes.com>

--- ecommerce_integrations/boot.py
+++ ecommerce_integrations/boot.py
@@ -1,12 +1,9 @@
-import frappe
-
-from ecommerce_integrations.shopify.constants import OLD_SETTINGS_DOCTYPE, SETTING_DOCTYPE
+from ecommerce_integrations.shopify.constants import OLD_SETTINGS_DOCTYPE
 
 
 def boot_session(bootinfo):
 	"""Don't show old doctypes after enabling new ones."""
-	if frappe.get_cached_value(SETTING_DOCTYPE, SETTING_DOCTYPE, "enable_shopify"):
-		try:
-			bootinfo.single_types.remove(OLD_SETTINGS_DOCTYPE)
-		except ValueError:
-			pass
+	try:
+		bootinfo.single_types.remove(OLD_SETTINGS_DOCTYPE)
+	except ValueError:
+		pass
