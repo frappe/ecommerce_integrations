3ea1e3efd781878aaf2a4aac7744e6b38bdb735e
Ankush Menat <ankush@frappe.io>
2021-10-28 18:01:20 +0530

    fix(shopify): don't run migration before enabling
    While harmless, this is getting triggered during install which can cause
    failure.

--- ecommerce_integrations/shopify/doctype/shopify_setting/shopify_setting.py
+++ ecommerce_integrations/shopify/doctype/shopify_setting/shopify_setting.py
@@ -47,7 +47,7 @@ class ShopifySetting(SettingController):
 			setup_custom_fields()
 
 	def on_update(self):
-		if not self.is_old_data_migrated:
+		if self.is_enabled() and not self.is_old_data_migrated:
 			migrate_from_old_connector()
 
 	def _handle_webhooks(self):
