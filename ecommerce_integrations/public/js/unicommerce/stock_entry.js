4416f570deae22ccbee3916fdbf0c61b12bc525a
Ankush Menat <ankush@frappe.io>
2021-12-06 19:33:04 +0530

    feat(ux): open grn button
    [skip ci]

---
+++ ecommerce_integrations/public/js/unicommerce/stock_entry.js
@@ -0,0 +1,25 @@
+frappe.ui.form.on("Stock Entry", {
+	refresh(frm) {
+		if (frm.doc.stock_entry_type == "GRN on Unicommerce") {
+			frm.add_custom_button(
+				__("Open GRNs"),
+				function () {
+					frappe.call({
+						method:
+							"ecommerce_integrations.unicommerce.utils.get_unicommerce_document_url",
+						args: {
+							code: '',
+							doctype: frm.doc.doctype,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								window.open(r.message, "_blank");
+							}
+						},
+					});
+				},
+				__("Unicommerce")
+			);
+		}
+	},
+});
