6baa4c620502a6640d0f8f9dbe6b12f98389ba28
Anupam Kumar <anupamvns0099@gmail.com>
2021-12-10 12:06:53 +0530

    fix: sales invoice syncing issue (#143)

--- ecommerce_integrations/zenoti/doctype/zenoti_settings/zenoti_settings.py
+++ ecommerce_integrations/zenoti/doctype/zenoti_settings/zenoti_settings.py
@@ -76,10 +76,10 @@ def sync_invoices(center_id=None, start_date=None, end_date=None):
 					last_sync and get_datetime() > get_datetime(add_to_date(last_sync, hours=cint(interval)))
 				) or (start_date and end_date):
 					error_logs = []
+					process_sales_invoices(center, error_logs, start_date, end_date)
 					if not last_sync or get_datetime(last_sync) < get_datetime(end_date):
-						process_sales_invoices(center, error_logs, start_date, end_date)
 						center.db_set("last_sync", get_datetime(end_date))
-						frappe.db.commit()
+					frappe.db.commit()
 					if len(error_logs):
 						make_error_log(error_logs)
 
