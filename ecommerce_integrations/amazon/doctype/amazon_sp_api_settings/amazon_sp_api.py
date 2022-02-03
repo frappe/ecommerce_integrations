627a44142f21edf38c8901f0a5626ec65aea80a3
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-03 15:48:40 +0530

    chore: add license attribution

--- ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api.py
+++ ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api.py
@@ -56,6 +56,18 @@ MARKETPLACES = {
 	},
 }
 
+# Following code is adapted from https://github.com/andrewjroth/requests-auth-aws-sigv4 under the Apache License 2.0 with minor changes.
+
+# Copyright 2020 Andrew J Roth <andrew@andrewjroth.com>
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
 
 class AWSSigV4(AuthBase):
 	def __init__(self, service, **kwargs):
