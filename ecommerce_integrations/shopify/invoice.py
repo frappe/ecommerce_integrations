bd2dbecdadfc243ea8568b224b5ece1fa3335ffb
KerollesFathy <kerolles.f@outlook.com>
2023-02-13 11:42:38 +0200

    Added New child table Payment Gateways & when create PE against Si map bank_account to gateway_account.

--- ecommerce_integrations/shopify/invoice.py
+++ ecommerce_integrations/shopify/invoice.py
@@ -52,7 +52,7 @@ def create_sales_invoice(shopify_order, setting, so):
 		sales_invoice.insert(ignore_mandatory=True)
 		sales_invoice.submit()
 		if sales_invoice.grand_total > 0:
-			make_payament_entry_against_sales_invoice(sales_invoice, setting, posting_date)
+			make_payament_entry_against_sales_invoice(sales_invoice, setting, shopify_order, posting_date)
 
 		if shopify_order.get("note"):
 			sales_invoice.add_comment(text=f"Order Note: {shopify_order.get('note')}")
@@ -63,13 +63,23 @@ def set_cost_center(items, cost_center):
 		item.cost_center = cost_center
 
 
-def make_payament_entry_against_sales_invoice(doc, setting, posting_date=None):
+def make_payament_entry_against_sales_invoice(doc, settings, shopify_order, posting_date=None):
 	from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
+	_bank_account = setting.get('cash_bank_account')
+	
+	if len(shopify_order.get('payment_gateway_names')):
+		doc_gatewaty = shopify_order.get('payment_gateway_names')[0]
 
-	payment_entry = get_payment_entry(doc.doctype, doc.name, bank_account=setting.cash_bank_account)
+	if len(setting.get("payment_gateways")):
+		for gateway in setting.get("payment_gateways"):
+			if doc_gatewaty == gateway.get("gateway_name"):
+				_bank_account = gateway.get("gateway_account")
+
+	payment_entry = get_payment_entry(doc.doctype, doc.name, bank_account=_bank_account)
 	payment_entry.flags.ignore_mandatory = True
 	payment_entry.reference_no = doc.name
 	payment_entry.posting_date = posting_date or nowdate()
 	payment_entry.reference_date = posting_date or nowdate()
 	payment_entry.insert(ignore_permissions=True)
 	payment_entry.submit()
+	frappe.db.commit()
