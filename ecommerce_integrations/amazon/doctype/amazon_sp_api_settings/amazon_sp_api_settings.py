ffb7c97ad79d5888b0f9358c212356932b6f2db6
s-aga-r <sagarsharma.s312@gmail.com>
2023-08-03 15:02:04 +0530

    fix: unable to save `Amazon SP API Settings` (#262)
    * fix(ux): don't set default field map if duplicated
    
    * fix: unable to save `Amazon SP API Settings`

--- ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api_settings.py
+++ ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api_settings.py
@@ -31,9 +31,7 @@ class AmazonSPAPISettings(Document):
 		elif self.max_retry_limit and self.max_retry_limit > 5:
 			frappe.throw(frappe._("Value for <b>Max Retry Limit</b> must be less than or equal to 5."))
 
-	def save(self):
-		super(Document, self).save()
-
+	def after_save(self):
 		if not self.is_old_data_migrated:
 			migrate_old_data()
 			self.db_set("is_old_data_migrated", 1)
