47de9cfe5fc13fa8721c93666d81e0a88df83e14
SowmyaArunachalam <sowmyaarunachalam57@gmail.com>
2026-02-03 18:15:21 +0530

    chore: fix linters

--- ecommerce_integrations/unicommerce/tests/utils.py
+++ ecommerce_integrations/unicommerce/tests/utils.py
@@ -30,9 +30,6 @@ class TestCase(IntegrationTestCase):
 		# Call parent first to auto-generate standard test records
 		super().setUpClass()
 
-		# Then create our custom test data
-		cls.create_test_data()
-
 		# Now configure Unicommerce settings
 		settings = frappe.get_doc(SETTINGS_DOCTYPE)
 
@@ -60,54 +57,6 @@ class TestCase(IntegrationTestCase):
 		setup_custom_fields()
 		_setup_test_item_categories()
 		frappe.db.set_value("Stock Settings", None, "allow_negative_stock", 1)
-
-	@classmethod
-	def create_test_data(cls):
-		"""Create required test data BEFORE parent setUpClass"""
-		from ecommerce_integrations.tests.utils import EcommerceTestSuite
-
-		# Create prerequisite data
-		EcommerceTestSuite.create_warehouse_types()
-		EcommerceTestSuite.create_customer_groups()
-		EcommerceTestSuite.create_test_company()
-		EcommerceTestSuite.create_wp_warehouses()
-
-		# Create Unicommerce Channels manually BEFORE parent setUpClass
-		cls.create_unicommerce_channels()
-		frappe.db.commit()
-
-	@classmethod
-	def create_unicommerce_channels(cls):
-		"""Create test Unicommerce Channels"""
-		channels = [
-			{
-				"doctype": "Unicommerce Channel",
-				"channel_id": "RAINFOREST",
-				"display_name": "Amazon",
-				"enabled": 1,
-				"company": "Wind Power LLC",
-				"warehouse": "Stores - WP",
-				"return_warehouse": "Stores - WP",
-				"customer_group": "Individual",
-				"fnf_account": "Freight and Forwarding Charges - WP",
-				"cod_account": "Freight and Forwarding Charges - WP",
-				"igst_account": "Output Tax GST - WP",
-				"cgst_account": "Output Tax GST - WP",
-				"sgst_account": "Output Tax GST - WP",
-				"ugst_account": "Output Tax GST - WP",
-				"tcs_account": "Output Tax GST - WP",
-				"cost_center": "Main - WP",
-				"cash_or_bank_account": "Cash - WP",
-				"gift_wrap_account": "Miscellaneous Expenses - WP",
-			},
-		]
-
-		for channel_data in channels:
-			if not frappe.db.exists("Unicommerce Channel", channel_data["channel_id"]):
-				try:
-					frappe.get_doc(channel_data).insert(ignore_if_duplicate=True)
-				except Exception as e:
-					print(f"Failed to create channel: {e}")
 
 	@classmethod
 	def tearDownClass(cls):
