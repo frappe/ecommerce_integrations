9044d7187f4ea48fc1fa8294d042c0cf83b16612
ravibharathi656 <ravibharathi656@gmail.com>
2026-04-17 12:37:37 +0530

    fix(unicommerce): dn creation

--- ecommerce_integrations/unicommerce/delivery_note.py
+++ ecommerce_integrations/unicommerce/delivery_note.py
@@ -46,52 +46,16 @@ def prepare_delivery_note():
 
 
 def create_delivery_note(so, sales_invoice):
-	try:
-		# Create the delivery note
-		from frappe.model.mapper import make_mapped_doc
+	# Create the delivery note
+	from erpnext.selling.doctype.sales_order.sales_order import make_delivery_note
 
-		res = make_mapped_doc(
-			method="erpnext.selling.doctype.sales_order.sales_order.make_delivery_note", source_name=so.name
-		)
-		res.update({"items": []})
-		for item in sales_invoice.items:
-			res.append(
-				"items",
-				{
-					"item_code": item.item_code,
-					"item_name": item.item_name,
-					"description": item.description,
-					"qty": item.qty,
-					"uom": item.uom,
-					"rate": item.rate,
-					"amount": item.amount,
-					"warehouse": item.warehouse,
-					"against_sales_order": item.sales_order,
-					"batch_no": item.batch_no,
-					"so_detail": item.so_detail,
-				},
-			)
-		for item in sales_invoice.taxes:
-			res.append(
-				"taxes",
-				{
-					"charge_type": item.charge_type,
-					"account_head": item.account_head,
-					"tax_amount": item.tax_amount,
-					"description": item.description,
-					"item_wise_tax_detail": item.item_wise_tax_detail,
-					"dont_recompute_tax": item.dont_recompute_tax,
-				},
-			)
-		res.unicommerce_order_code = sales_invoice.unicommerce_order_code
-		res.unicommerce_shipment_id = sales_invoice.unicommerce_shipping_package_code
-		res.save()
-		res.submit()
-		log = create_unicommerce_log(method="create_delevery_note", make_new=True)
-		frappe.flags.request_id = log.name
-	except Exception as e:
-		create_unicommerce_log(status="Error", exception=e, rollback=True)
-	else:
-		create_unicommerce_log(status="Success")
-		frappe.flags.request_id = None
-		return res
+	res = make_delivery_note(source_name=so.name)
+	res.unicommerce_order_code = sales_invoice.unicommerce_order_code
+	res.unicommerce_shipment_id = sales_invoice.unicommerce_shipping_package_code
+	res.save()
+	res.submit()
+	log = create_unicommerce_log(method="create_delevery_note", make_new=True)
+	frappe.flags.request_id = log.name
+	create_unicommerce_log(status="Success")
+	frappe.flags.request_id = None
+	return res
